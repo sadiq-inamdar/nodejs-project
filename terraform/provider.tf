b0b108fdbc3af773c57ba858745ae8e01d250bd7
sadiq-inamdar <sadeeq8951@gmail.com>
2025-07-06 18:19:12 +0530

    debug

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,13 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+
   backend "s3" {
     bucket         = "nodejs-statefile"
     key            = "eks/terraform.tfstate"
