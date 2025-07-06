1e017f587ad027aa019dbc3f9bcdb80709adef52
sadiq-inamdar <sadeeq8951@gmail.com>
2025-07-06 21:56:36 +0530

    debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,11 @@ data "aws_subnets" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+  
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+  }
 }
 
 module "eks" {
