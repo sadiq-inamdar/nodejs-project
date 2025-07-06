c90096d647312506a7fb2862ee95cc6efe159c9b
sadiq-inamdar <sadeeq8951@gmail.com>
2025-07-06 18:21:44 +0530

    debug

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,12 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
   }
-
-  required_version = ">= 1.3.0"
 
   backend "s3" {
     bucket         = "nodejs-statefile"
